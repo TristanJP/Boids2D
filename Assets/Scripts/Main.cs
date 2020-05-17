de2104876342ea0ec496b5b14e80138d72e1e4e5
TristanJP <trisperk@hotmail.com>
2020-05-17 18:22:33 +0100

    Mde text change with count

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -16,6 +16,7 @@ public class Main : MonoBehaviour
 
     public ColorPicker colorPickerComp;
     public Slider boidCountSlider;
+    public Text boidCountText;
 
     // Start is called before the first frame update
     void Start()
@@ -109,6 +110,7 @@ public class Main : MonoBehaviour
             if (boidCount > boidList.Count) {
                 addBoid(false);
             }
+            boidCountText.text = ""+ boidCount;
         }
     }
 }
